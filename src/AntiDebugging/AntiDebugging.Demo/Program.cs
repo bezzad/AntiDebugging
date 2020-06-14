85cc6d4513dce7570aa91ce27b03ea04f82e6b05
Behzad <behzad.khosravifar@gmail.com>
2020-06-14 13:14:31 +0430

    Fixed demo app

--- src/AntiDebugging/AntiDebugging.Demo/Program.cs
+++ src/AntiDebugging/AntiDebugging.Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace AntiDebugging.Demo
 {
@@ -7,9 +8,11 @@ namespace AntiDebugging.Demo
     {
         static void Main(string[] args)
         {
+            var mainLibVersion = Assembly.GetAssembly(typeof(AntiDebugging.AntiDebug))?.GetName()?.Version.ToString(3) ?? "1.0.0";
+            Console.Title = $"Anti Debugging v{mainLibVersion}";
             Console.WriteLine($"Process {Process.GetCurrentProcess().Id} is running...");
-            var ip = HardwareHelper.Ip();
-            Console.WriteLine($"PC is from Internet Protocol Address: {ip}");
+            //            var ip = HardwareHelper.Ip();
+            //          Console.WriteLine($"PC is from Internet Protocol Address: {ip}");
 
             if (PerformChecks())
             {
@@ -17,19 +20,18 @@ namespace AntiDebugging.Demo
             }
             else
             {
-                AntiDump.ProtectDump();
-                Scanner.ScanAndKill(() => Console.WriteLine("Scan and kill system any malware"));
-
-                Console.WriteLine("Test: Attaching to process in runtime...");
-                var ppid = 0;
-                if (args != null && args.Length > 0)
-                {
-                    int.TryParse(args[0], out ppid);
-                }
-
-                SelfDebugger.DebugSelf(ppid);
-
-                PerformChecks();
+                // AntiDump.ProtectDump();
+                //
+                // Console.WriteLine("Test: Attaching to process in runtime...");
+                // var ppid = 0;
+                // if (args != null && args.Length > 0)
+                // {
+                //     int.TryParse(args[0], out ppid);
+                // }
+                //
+                // SelfDebugger.DebugSelf(ppid);
+                //
+                // PerformChecks();
             }
 
             Console.ReadLine();
@@ -50,16 +52,17 @@ namespace AntiDebugging.Demo
             var detectSandbox = ProtectionHelper.DetectSandbox();
             var detectVirtualMachine = ProtectionHelper.DetectVirtualMachine();
             var detachFromDebuggerProcess = AntiDebug.DetachFromDebuggerProcess();
-            Console.WriteLine($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckRemoteDebugger)}: {isProcessRemote}");
-            Console.WriteLine($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckDebuggerManagedPresent)}: {isManagedCodesAttached}");
-            Console.WriteLine($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckDebuggerUnmanagedPresent)}: {isUnManagedCodesAttached}");
-            Console.WriteLine($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckDebugPort)}: {checkDebugPort}");
-            Console.WriteLine($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckKernelDebugInformation)}: {checkKernelDebugInformation}");
-            Console.WriteLine($"{nameof(ProtectionHelper)}.{nameof(ProtectionHelper.DetectEmulation)}: {detectEmulation}");
-            Console.WriteLine($"{nameof(ProtectionHelper)}.{nameof(ProtectionHelper.DetectSandbox)}: {detectSandbox}");
-            Console.WriteLine($"{nameof(ProtectionHelper)}.{nameof(ProtectionHelper.DetectVirtualMachine)}: {detectVirtualMachine}");
-            Console.WriteLine($"{nameof(AntiDebug)}.{nameof(AntiDebug.DetachFromDebuggerProcess)}: {detachFromDebuggerProcess}");
+            WriteResult($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckRemoteDebugger)}", isProcessRemote);
+            WriteResult($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckDebuggerManagedPresent)}", isManagedCodesAttached);
+            WriteResult($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckDebuggerUnmanagedPresent)}", isUnManagedCodesAttached);
+            WriteResult($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckDebugPort)}", checkDebugPort);
+            WriteResult($"{nameof(AntiDebug)}.{nameof(AntiDebug.CheckKernelDebugInformation)}", checkKernelDebugInformation);
+            WriteResult($"{nameof(ProtectionHelper)}.{nameof(ProtectionHelper.DetectEmulation)}", detectEmulation);
+            WriteResult($"{nameof(ProtectionHelper)}.{nameof(ProtectionHelper.DetectSandbox)}", detectSandbox);
+            WriteResult($"{nameof(ProtectionHelper)}.{nameof(ProtectionHelper.DetectVirtualMachine)}", detectVirtualMachine);
+            WriteResult($"{nameof(AntiDebug)}.{nameof(AntiDebug.DetachFromDebuggerProcess)}", detachFromDebuggerProcess);
             AntiDebug.HideOsThreads();
+            Scanner.ScanAndKill(() => Console.WriteLine("Scan and kill system any malware"));
 
             if (isProcessRemote || isManagedCodesAttached || isUnManagedCodesAttached)
             {
@@ -70,5 +73,17 @@ namespace AntiDebugging.Demo
 
             return false;
         }
+
+        protected static void WriteResult(string prop, bool value)
+        {
+            Console.Write($"{prop}: ");
+            var originalColor = Console.ForegroundColor;
+            if (value)
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(value);
+            Console.ForegroundColor = originalColor;
+        }
     }
+
 }
